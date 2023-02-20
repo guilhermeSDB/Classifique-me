d5814a59090382a959d788f7e8e04047ee8a1cff
Guilherme S <guilherme_pikapal@hotmail.com>
2023-02-19 21:31:30 -0300

    feat: Traduzido para PT-BR

--- js/app.js
+++ js/app.js
@@ -7,40 +7,38 @@ const classificationElement = document.getElementsByClassName("classification");
 let erroElement = document.getElementsByClassName("error");
 let valor = 0;
 
-opcaoSpread.forEach(function(element){
-    element.addEventListener("click",function(){
-        opcaoSpread.forEach(function(el){
-            el.classList.remove("active");
-        })
-        if(erroElement.length > 0){
-            erroElement[0].remove();
-        }
-        valor = element.dataset.value;
-        element.classList.add("active");
-
+opcaoSpread.forEach(function (element) {
+  element.addEventListener("click", function () {
+    opcaoSpread.forEach(function (el) {
+      el.classList.remove("active");
     });
+    if (erroElement.length > 0) {
+      erroElement[0].remove();
+    }
+    valor = element.dataset.value;
+    element.classList.add("active");
+  });
 });
 
-buttonSubmit.addEventListener('click', function(){
-    if(valor > 0){
-        ratingElement.style.display = "none";
-        const ilustrationElement = document.querySelector(".ilustration")
-        let text = `You selected ${valor} out of 5`;
-        createElement("div", text, "classification-result", ilustrationElement)
+buttonSubmit.addEventListener("click", function () {
+  if (valor > 0) {
+    ratingElement.style.display = "none";
+    const ilustrationElement = document.querySelector(".ilustration");
+    let text = `Você selecionou ${valor} de 5`;
+    createElement("div", text, "classification-result", ilustrationElement);
 
-        thanksElement.style.display = "block";
-
-    }else{
-        if(erroElement.length == 0){
-            let text = "Selecione uma avaliação primeiro";
-            createElement("div", text, "error", buttonSubmit)
-        }
+    thanksElement.style.display = "block";
+  } else {
+    if (erroElement.length == 0) {
+      let text = "Selecione uma avaliação primeiro";
+      createElement("div", text, "error", buttonSubmit);
     }
-})
+  }
+});
 
-function createElement(type, text, classes, elementToInsertAfter){
-    let divElement = document.createElement(type);
-    divElement.innerText = text;
-    divElement.classList.add(classes);
-    elementToInsertAfter.after(divElement)
+function createElement(type, text, classes, elementToInsertAfter) {
+  let divElement = document.createElement(type);
+  divElement.innerText = text;
+  divElement.classList.add(classes);
+  elementToInsertAfter.after(divElement);
 }
